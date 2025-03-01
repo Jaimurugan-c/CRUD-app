b3e537c3433b7de01ced8bcb7e749bfa27641490
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-03-01 12:58:57 +0530

    Create-User completed

--- CRUD/src/User.jsx
+++ CRUD/src/User.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 
 const User = () => {
+  const [users,setUsers] = useState([{
+    Name: 'Jai', Email:'Jai@gmail.com', Age: 20
+  }])
   return (
-    <div className='d-flex vh-100 
-    justify-content-center bg-primary align-items-center'>
-      <div className='w-50 bg-white rounded p-3'>
-        <table className='table'>
-            <thead>
+    <div className='d-flex vh-100 justify-content-center bg-primary align-items-center'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-12 col-md-8 col-lg-6 bg-white rounded p-3'>
+            <table className='table'>
+              <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Age</th>
+                  <th>Actions</th>
                 </tr>
-                </thead>
-                <tbody>
-                    
-                </tbody>
+              </thead>
+              <tbody>
+                {users.map((user) => {
+                  return <tr>
+                    <td>{user.Name}</td>
+                    <td>{user.Email}</td>
+                    <td>{user.Age}</td>
+                    <td><button className='bg-success 
+                    btn  text-light '>Edit</button><button className='bg-danger btn  text-light'>Delete</button></td>
+                  </tr>
+                })}
+              </tbody>
             </table>
+          </div>
+        </div>
       </div>
     </div>
   )
